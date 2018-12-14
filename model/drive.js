5326ce2e783f0141e7a0901b16d88994da1c01ed
aniolg <aniol_95@hotmail.com>
2018-12-14 12:33:16 +0100

    correction increment/decrement value (any number) in decrease/increas…

--- model/drive.js
+++ model/drive.js
@@ -4,17 +4,17 @@ var Drive = (function () {
         this.id = id;
         this.maxPower = maxPower;
     }
-    Drive.prototype.increasePower = function () {
+    Drive.prototype.increasePower = function (increment) {
         if (this.actualPower < this.maxPower) {
-            this.actualPower += 10;
+            this.actualPower += increment;
         }
         else {
             alert("No es pot accelerar m\u00E9s el propulsor! La seva Pot\u00E8ncia M\u00E0xima \u00E9s: " + this.maxPower);
         }
     };
-    Drive.prototype.decreasePower = function () {
+    Drive.prototype.decreasePower = function (decrement) {
         if (this.actualPower > 0) {
-            this.actualPower -= 10;
+            this.actualPower -= decrement;
         }
         else {
             alert("El propulsor ja est\u00E0 a Pot\u00E8ncia 0.");
