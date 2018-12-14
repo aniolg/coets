98b90a823a8ae5719652275c11045ee736f6f53d
aniolg <aniol_95@hotmail.com>
2018-12-14 12:33:39 +0100

    correction increment/decrement value (any number) in accelerarte/slow…

--- model/rocket.js
+++ model/rocket.js
@@ -1,48 +1,45 @@
 var Rocket = (function () {
     function Rocket(id) {
         this.drives = [];
-        this.actualVelocity = 0;
         this.id = id;
         this.idChecker(id);
         this.checkedId = this.idChecker(id);
         this.numDrives = 0;
     }
     Rocket.prototype.idChecker = function (idIn) {
-        var e;
         if (idIn.length >= 8) {
             return idIn;
         }
         else {
-            throw e = new Error("La ID del coet " + idIn + " és massa curta! (introdueixi una ID de mínim 8 caràcters)");
-            return e;
+            throw new Error("La ID del coet " + idIn + " és massa curta! (introdueixi una ID de mínim 8 caràcters)");
         }
     };
     Rocket.prototype.addDrive = function (idDrive, maxPowerDrive) {
         this.drives.push(new Drive(idDrive, maxPowerDrive));
         ++this.numDrives;
     };
-    Rocket.prototype.accelerate = function () {
+    Rocket.prototype.accelerate = function (increment) {
         for (var i = 0; i < this.drives.length; i++) {
             if (this.drives[i].actualPower < this.drives[i].maxPower) {
-                this.drives[i].increasePower();
+                this.drives[i].increasePower(increment);
                 i = this.drives.length;
             }
         }
     };
-    Rocket.prototype.slowDown = function () {
+    Rocket.prototype.slowDown = function (decrement) {
         for (var i = this.drives.length - 1; i >= 0; i--) {
             if (this.drives[i].actualPower > 0) {
-                this.drives[i].decreasePower();
+                this.drives[i].decreasePower(decrement);
                 i = -1;
             }
         }
     };
     Rocket.prototype.getActualVelocity = function () {
-        this.actualVelocity = 0;
+        var actualVelocity = 0;
         for (var i = 0; i < this.drives.length; i++) {
-            this.actualVelocity += this.drives[i].actualPower;
+            actualVelocity += this.drives[i].actualPower;
         }
-        return this.actualVelocity;
+        return actualVelocity;
     };
     return Rocket;
 }());
