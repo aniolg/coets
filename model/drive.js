5e8aec4270acdfa49da2d49f754cbb3fa80dacc3
aniolg <aniol_95@hotmail.com>
2018-12-17 11:07:32 +0100

    simplifyed increase/deacrease power functions

--- model/drive.js
+++ model/drive.js
@@ -5,27 +5,17 @@ var Drive = (function () {
         this.maxPower = maxPower;
     }
     Drive.prototype.increasePower = function (increment) {
-        if (this.actualPower < this.maxPower && increment <= this.maxPower) {
-            this.actualPower += increment;
-        }
-        else if (this.actualPower < this.maxPower && increment > this.maxPower) {
-            alert("Ha hagut un increment de " + (this.actualPower - this.maxPower) + ". El propulsor ha arribat a la seva Pot\u00E8ncia M\u00E0xima de " + this.maxPower);
+        this.actualPower += increment;
+        if (this.actualPower > this.maxPower) {
             this.actualPower = this.maxPower;
-        }
-        else {
-            alert("No es pot accelerar m\u00E9s el propulsor! La seva Pot\u00E8ncia M\u00E0xima \u00E9s: " + this.maxPower);
+            alert("El propulsor ja no pot incrementar m\u00E9s la seva pot\u00E8ncia. Ha arribat a la seva Pot\u00E8ncia M\u00E0xima de " + this.maxPower);
         }
     };
     Drive.prototype.decreasePower = function (decrement) {
-        if (this.actualPower > 0 && decrement <= this.actualPower) {
-            this.actualPower -= decrement;
-        }
-        else if (this.actualPower > 0 && decrement > this.actualPower) {
-            alert("Hi ha hagut un decremnt de " + this.actualPower + ". El propulsor ja est\u00E0 a Pot\u00E8ncia 0.");
+        this.actualPower -= decrement;
+        if (this.actualPower < 0) {
             this.actualPower = 0;
-        }
-        else {
-            alert("El propulsor ja est\u00E0 a Pot\u00E8ncia 0.");
+            alert("El propulsor est\u00E0 a Pot\u00E8ncia 0. Ja no pot disminuir m\u00E9s.");
         }
     };
     return Drive;
--- model/drive.ts
+++ model/drive.ts
@@ -9,25 +9,19 @@ class Drive {
         this.maxPower = maxPower;
     }
 
-    increasePower(increment:number) {
-        if (this.actualPower < this.maxPower && increment <= this.maxPower) {
-            this.actualPower += increment;
-        } else if(this.actualPower < this.maxPower && increment > this.maxPower){
-            alert(`Ha hagut un increment de ${this.actualPower - this.maxPower}. El propulsor ha arribat a la seva Potència Màxima de ${this.maxPower}`);
-            this.actualPower = this.maxPower;
-        } else {
-            alert(`No es pot accelerar més el propulsor! La seva Potència Màxima és: ${this.maxPower}`);
+    increasePower(increment:number) { 
+        this.actualPower += increment;
+        if (this.actualPower > this.maxPower) {
+           this.actualPower = this.maxPower;
+           alert(`El propulsor ja no pot incrementar més la seva potència. Ha arribat a la seva Potència Màxima de ${this.maxPower}`);
         }
     }
 
     decreasePower(decrement:number) {
-        if (this.actualPower > 0 && decrement <= this.actualPower) {
-            this.actualPower -= decrement;
-        } else if (this.actualPower > 0 && decrement > this.actualPower){
-            alert(`Hi ha hagut un decremnt de ${this.actualPower}. El propulsor ja està a Potència 0.`);
+        this.actualPower -= decrement;
+        if (this.actualPower < 0){
             this.actualPower = 0;
-        }else {
-            alert(`El propulsor ja està a Potència 0.`);
+            alert(`El propulsor està a Potència 0. Ja no pot disminuir més.`);
         }
     }
 
