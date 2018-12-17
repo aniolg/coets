e9af0a851702554df6d83e114696cf9983f1ba0e
aniolg <aniol_95@hotmail.com>
2018-12-17 10:50:21 +0100

    solved incremnt/decrement error

--- model/drive.js
+++ model/drive.js
@@ -8,6 +8,10 @@ var Drive = (function () {
         if (this.actualPower < this.maxPower && increment <= this.maxPower) {
             this.actualPower += increment;
         }
+        else if (this.actualPower < this.maxPower && increment > this.maxPower) {
+            alert("Ha hagut un increment de " + (this.actualPower - this.maxPower) + ". El propulsor ha arribat a la seva Pot\u00E8ncia M\u00E0xima de " + this.maxPower);
+            this.actualPower = this.maxPower;
+        }
         else {
             alert("No es pot accelerar m\u00E9s el propulsor! La seva Pot\u00E8ncia M\u00E0xima \u00E9s: " + this.maxPower);
         }
@@ -17,6 +21,7 @@ var Drive = (function () {
             this.actualPower -= decrement;
         }
         else if (this.actualPower > 0 && decrement > this.actualPower) {
+            alert("Hi ha hagut un decremnt de " + this.actualPower + ". El propulsor ja est\u00E0 a Pot\u00E8ncia 0.");
             this.actualPower = 0;
         }
         else {
--- model/drive.ts
+++ model/drive.ts
@@ -12,6 +12,9 @@ class Drive {
     increasePower(increment:number) {
         if (this.actualPower < this.maxPower && increment <= this.maxPower) {
             this.actualPower += increment;
+        } else if(this.actualPower < this.maxPower && increment > this.maxPower){
+            alert(`Ha hagut un increment de ${this.actualPower - this.maxPower}. El propulsor ha arribat a la seva Potència Màxima de ${this.maxPower}`);
+            this.actualPower = this.maxPower;
         } else {
             alert(`No es pot accelerar més el propulsor! La seva Potència Màxima és: ${this.maxPower}`);
         }
@@ -21,6 +24,7 @@ class Drive {
         if (this.actualPower > 0 && decrement <= this.actualPower) {
             this.actualPower -= decrement;
         } else if (this.actualPower > 0 && decrement > this.actualPower){
+            alert(`Hi ha hagut un decremnt de ${this.actualPower}. El propulsor ja està a Potència 0.`);
             this.actualPower = 0;
         }else {
             alert(`El propulsor ja està a Potència 0.`);
